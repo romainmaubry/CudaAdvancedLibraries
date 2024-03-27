40a7f2f555f041a1db23a0b67df755745bfb045b
Romain Aubry <romain.m.aubry@gmail.com>
2024-03-27 20:38:29 +0000

    First successfull execution

--- cnn.py
+++ cnn.py
@@ -1,5 +1,8 @@
 from keras.datasets import cifar10
+import matplotlib.pyplot as plt
 (X_train,y_train),(X_test,y_test)=cifar10.load_data()
+
+#Normalize data
 X_train=X_train.astype('float32')/255
 X_test=X_test.astype('float32')/255
 
@@ -25,13 +28,26 @@ model.add(Flatten())
 model.add(Dense(512,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
+model.summary()
+
+chart_x = []
+chart_y_train = []
+chart_y_test = []
 
 #Training
 
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-model.fit(X_train,y_train,batch_size=32,epochs=10,validation_data=(X_test,y_test))
+history=model.fit(X_train,y_train,batch_size=32,epochs=10,validation_data=(X_test,y_test))
 
 score=model.evaluate(X_test,y_test,verbose=0)
 print('Test loss:',score[0])
 print('Test accuracy:',score[1])
 
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+plt.savefig('cnn.png')
