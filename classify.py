f22b738d5f31057e26f902e25fe76a7e52608424
Romain Aubry <romain.m.aubry@gmail.com>
2024-03-27 20:51:02 +0000

    Add comments

--- classify.py
+++ classify.py
@@ -1,20 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import idx2numpy
+
+#Initialize seed
 np.random.seed(7)
+
+#Initialize learning rate
 LEARNING_RATE= 0.01
+
+#Initialize lepochs number
 EPOCHS = 20
+
+#Initialize data name
 TRAIN_IMAGE_FILENAME='data/mnist/train-images-idx3-ubyte'
 TRAIN_LABEL_FILENAME='data/mnist/train-labels-idx1-ubyte'
 TEST_IMAGE_FILENAME='data/mnist/t10k-images-idx3-ubyte'
 TEST_LABEL_FILENAME='data/mnist/t10k-labels-idx1-ubyte'
 
+#Read data
 def read_mnist():
     train_images = idx2numpy.convert_from_file(TRAIN_IMAGE_FILENAME)
     train_labels = idx2numpy.convert_from_file(TRAIN_LABEL_FILENAME)
     test_images  = idx2numpy.convert_from_file(TEST_IMAGE_FILENAME)
     test_labels  = idx2numpy.convert_from_file(TEST_LABEL_FILENAME)
 
+    #Reshape as 2D array and normalize
     x_train = train_images.reshape(60000,784)
     mean = np.mean(x_train)
     stddev = np.std(x_train)
@@ -31,10 +41,13 @@ def read_mnist():
         y_test[i][y]=1
     return x_train,y_train,x_test,y_test
 
-
+#Get data
 x_train,y_train,x_test,y_test = read_mnist()
+
+#Used for random order
 index_list = list(range(len(x_train)))
 
+#Initialize neurons
 def layer_w(neuron_count,input_count):
     weights = np.zeros((neuron_count,input_count+1))
     for i in range(1,neuron_count):
@@ -42,18 +55,23 @@ def layer_w(neuron_count,input_count):
             weights[i][j]=np.random.uniform(-0.1,0.1)
     return weights
 
+#Initialize neuron count for the hidden layer
 neuron_count=10
 hidden_layer_w = layer_w(neuron_count,784)
 hidden_layer_y = np.zeros(neuron_count)
 hidden_layer_error = np.zeros(neuron_count)
 
+#Initialize output layer
 output_layer_w = layer_w(10,neuron_count)
 output_layer_y = np.zeros(10)
 output_layer_error = np.zeros(10)
 
+#Initialize arrays for figure
 chart_x = []
 chart_y_train = []
 chart_y_test = []
+
+#Print improvement and remember progree to plot
 def show_learning(epoch_no,train_acc,test_acc):
     global chart_x
     global chart_y_train
@@ -74,6 +92,7 @@ def plot_learning():
     plt.savefig('train.png')
 
 
+#Forward pass
 def forward_pass(x):
     global hidden_layer_y
     global output_layer_y
@@ -86,6 +105,7 @@ def forward_pass(x):
         z = np.dot(w,hidden_output_array)
         output_layer_y[i] = 1.0/(1.0+np.exp(-z))
 
+#Backward pass
 def backward_pass(y_truth):
     global hidden_layer_error
     global output_layer_error
@@ -102,6 +122,7 @@ def backward_pass(y_truth):
         weighted_error = np.dot(error_weight_array,output_layer_error)
         hidden_layer_error[i]=weighted_error*derivative
 
+#Update weights
 def adjust_weights(x):
     global output_layer_w
     global hidden_layer_w
@@ -111,6 +132,7 @@ def adjust_weights(x):
     for i,error in enumerate(output_layer_error):
         output_layer_w[i]-=(hidden_output_array*LEARNING_RATE*error)
 
+#Main training loop
 for i in range(EPOCHS):
     np.random.shuffle(index_list)
     correct_training_results = 0
@@ -132,6 +154,7 @@ for i in range(EPOCHS):
             correct_test_results +=1
     show_learning(i,correct_training_results/len(x_train),correct_test_results/len(x_test))
 
+#Plot the results
 plot_learning()
             
 
--- cnn.py
+++ cnn.py
@@ -1,23 +1,24 @@
 from keras.datasets import cifar10
 import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense
+
+#Get model data
 (X_train,y_train),(X_test,y_test)=cifar10.load_data()
 
 #Normalize data
 X_train=X_train.astype('float32')/255
 X_test=X_test.astype('float32')/255
 
-from keras.utils import to_categorical
-
+#One hot encoding of the data
 y_train=to_categorical(y_train,10)
 y_test=to_categorical(y_test,10)
 
-
 #Architecture of the CNN
-from keras.models import Sequential
-from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense
-
 model =Sequential()
 
+#Add layers
 model.add(Conv2D(32,(3,3),activation='relu',padding='same',input_shape=(32,32,3)))
 model.add(Conv2D(32,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
@@ -28,21 +29,19 @@ model.add(Flatten())
 model.add(Dense(512,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
+#Print model summary
 model.summary()
 
-chart_x = []
-chart_y_train = []
-chart_y_test = []
-
-#Training
-
+#Train the model
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 history=model.fit(X_train,y_train,batch_size=32,epochs=10,validation_data=(X_test,y_test))
 
+#Evaluate the model
 score=model.evaluate(X_test,y_test,verbose=0)
 print('Test loss:',score[0])
 print('Test accuracy:',score[1])
 
+#Create figure
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('model loss')
