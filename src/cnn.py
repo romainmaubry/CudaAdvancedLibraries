1845249b7f8b197357ee0252db17b48563914ed1
Romain Aubry <romain.m.aubry@gmail.com>
2024-03-28 18:21:39 +0000

    Modifying the parameters

--- src/cnn.py
+++ src/cnn.py
@@ -26,7 +26,7 @@ model.add(Conv2D(64,(3,3),activation='relu',padding='same'))
 model.add(Conv2D(64,(3,3),activation='relu'))
 model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(512,activation='relu'))
+model.add(Dense(64,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
 #Print model summary
